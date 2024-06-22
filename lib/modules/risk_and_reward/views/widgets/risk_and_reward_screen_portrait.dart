8385a10718ddc75592063b2a83029299a92c08e2
JASHWANTH NEELA <jashwanthn066@gmail.com>
2024-06-21 23:25:49 -0500

    Colors updated

--- lib/modules/risk_and_reward/views/widgets/risk_and_reward_screen_portrait.dart
+++ lib/modules/risk_and_reward/views/widgets/risk_and_reward_screen_portrait.dart
@@ -20,19 +20,46 @@ class RiskAndRewardScreenPortrait extends StatelessWidget {
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
-            title: const Text('Risk and Reward'),
+            title: const Text(
+              'Risk and Reward',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            centerTitle: true,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.purple],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ),
           ),
-          body: state.isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : buildChartWidget(),
+          body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.white, Colors.lightBlueAccent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: state.isLoading
+                ? const Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                    ),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: buildChartWidget(state),
+                  ),
+          ),
         );
       },
     );
   }
 
-  Widget buildChartWidget() {
-    final state = _controller.state;
-
+  Widget buildChartWidget(RiskAndRewardState state) {
     final data =
         state.spots.map((spot) => ProfitLossPoint(spot.x, spot.y)).toList();
 
@@ -67,77 +94,110 @@ class RiskAndRewardScreenPortrait extends StatelessWidget {
     final double xMargin = (state.maxX - state.minX) * 0.2;
     final double yMargin = (state.maxY - state.minY) * 0.2;
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 500,
-            child: charts.LineChart(
-              series,
-              animate: true,
-              domainAxis: charts.NumericAxisSpec(
-                viewport: charts.NumericExtents(
-                    state.minX - xMargin, state.maxX + xMargin),
-                tickProviderSpec: charts.BasicNumericTickProviderSpec(
-                  desiredTickCount:
-                      ((state.maxX - state.minX + 2 * xMargin) / 0.5).ceil(),
-                  zeroBound: false,
+    return Column(
+      children: [
+        SizedBox(
+          height: 500,
+          child: charts.LineChart(
+            series,
+            animate: true,
+            domainAxis: charts.NumericAxisSpec(
+              viewport: charts.NumericExtents(
+                  state.minX - xMargin, state.maxX + xMargin),
+              tickProviderSpec: charts.BasicNumericTickProviderSpec(
+                desiredTickCount:
+                    ((state.maxX - state.minX + 2 * xMargin) / 0.5).ceil(),
+                zeroBound: false,
+              ),
+              renderSpec: charts.SmallTickRendererSpec(
+                labelStyle: const charts.TextStyleSpec(
+                  fontSize: 14,
+                  color: charts.MaterialPalette.black,
+                ),
+                lineStyle: charts.LineStyleSpec(
+                  color: charts.MaterialPalette.gray.shadeDefault,
                 ),
               ),
-              primaryMeasureAxis: charts.NumericAxisSpec(
-                viewport: charts.NumericExtents(
-                    state.minY - yMargin, state.maxY + yMargin),
-                tickProviderSpec: charts.BasicNumericTickProviderSpec(
-                  desiredTickCount:
-                      ((state.maxY - state.minY + 2 * yMargin) / 0.5).ceil(),
-                  zeroBound: false,
-                ),
-              ),
-              behaviors: [
-                charts.ChartTitle('Underlying Price',
-                    behaviorPosition: charts.BehaviorPosition.bottom,
-                    titleOutsideJustification:
-                        charts.OutsideJustification.middleDrawArea),
-                charts.ChartTitle('Profit / Loss',
-                    behaviorPosition: charts.BehaviorPosition.start,
-                    titleOutsideJustification:
-                        charts.OutsideJustification.middleDrawArea),
-                charts.RangeAnnotation(
-                  state.breakEvenPoints.map((point) {
-                    return charts.LineAnnotationSegment(
-                      point,
-                      charts.RangeAnnotationAxisType.domain,
-                      startLabel: 'Break-even',
-                      labelAnchor: charts.AnnotationLabelAnchor.end,
-                      color: charts.MaterialPalette.red.shadeDefault,
-                      dashPattern: [4, 4],
-                    );
-                  }).toList(),
-                ),
-                charts.RangeAnnotation([
-                  charts.LineAnnotationSegment(
-                      state.maxProfit, charts.RangeAnnotationAxisType.measure,
-                      startLabel:
-                          'Max Profit: ${state.maxProfit == double.infinity ? 'Unlimited' : state.maxProfit.toStringAsFixed(2)}',
-                      labelAnchor: charts.AnnotationLabelAnchor.start,
-                      color: charts.MaterialPalette.green.shadeDefault,
-                      dashPattern: [4, 4],
-                      strokeWidthPx: 3),
-                  charts.LineAnnotationSegment(
-                      state.maxLoss, charts.RangeAnnotationAxisType.measure,
-                      startLabel:
-                          'Max Loss: ${state.maxLoss == double.infinity ? 'Unlimited' : state.maxLoss.toStringAsFixed(2)}',
-                      labelAnchor: charts.AnnotationLabelAnchor.start,
-                      color: charts.MaterialPalette.red.shadeDefault,
-                      dashPattern: [4, 4],
-                      strokeWidthPx: 3),
-                ]),
-              ],
             ),
+            primaryMeasureAxis: charts.NumericAxisSpec(
+              viewport: charts.NumericExtents(
+                  state.minY - yMargin, state.maxY + yMargin),
+              tickProviderSpec: charts.BasicNumericTickProviderSpec(
+                desiredTickCount:
+                    ((state.maxY - state.minY + 2 * yMargin) / 0.5).ceil(),
+                zeroBound: false,
+              ),
+              renderSpec: charts.GridlineRendererSpec(
+                labelStyle: const charts.TextStyleSpec(
+                  fontSize: 14,
+                  color: charts.MaterialPalette.black,
+                ),
+                lineStyle: charts.LineStyleSpec(
+                  color: charts.MaterialPalette.gray.shadeDefault,
+                ),
+              ),
+            ),
+            behaviors: [
+              charts.ChartTitle(
+                'Underlying Price',
+                behaviorPosition: charts.BehaviorPosition.bottom,
+                titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea,
+                titleStyleSpec: const charts.TextStyleSpec(
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  color: charts.MaterialPalette.black,
+                ),
+              ),
+              charts.ChartTitle(
+                'Profit / Loss',
+                behaviorPosition: charts.BehaviorPosition.start,
+                titleOutsideJustification:
+                    charts.OutsideJustification.middleDrawArea,
+                titleStyleSpec: const charts.TextStyleSpec(
+                  fontSize: 16,
+                  fontWeight: 'bold',
+                  color: charts.MaterialPalette.black,
+                ),
+              ),
+              charts.RangeAnnotation(
+                state.breakEvenPoints.map((point) {
+                  return charts.LineAnnotationSegment(
+                    point,
+                    charts.RangeAnnotationAxisType.domain,
+                    startLabel: 'Break-even',
+                    labelAnchor: charts.AnnotationLabelAnchor.end,
+                    color: charts.MaterialPalette.red.shadeDefault,
+                    dashPattern: [4, 4],
+                  );
+                }).toList(),
+              ),
+              charts.RangeAnnotation([
+                charts.LineAnnotationSegment(
+                  state.maxProfit,
+                  charts.RangeAnnotationAxisType.measure,
+                  startLabel:
+                      'Max Profit: ${state.maxProfit == double.infinity ? 'Unlimited' : state.maxProfit.toStringAsFixed(2)}',
+                  labelAnchor: charts.AnnotationLabelAnchor.start,
+                  color: charts.MaterialPalette.green.shadeDefault,
+                  dashPattern: [4, 4],
+                  strokeWidthPx: 3,
+                ),
+                charts.LineAnnotationSegment(
+                  state.maxLoss,
+                  charts.RangeAnnotationAxisType.measure,
+                  startLabel:
+                      'Max Loss: ${state.maxLoss == double.infinity ? 'Unlimited' : state.maxLoss.toStringAsFixed(2)}',
+                  labelAnchor: charts.AnnotationLabelAnchor.start,
+                  color: charts.MaterialPalette.red.shadeDefault,
+                  dashPattern: [4, 4],
+                  strokeWidthPx: 3,
+                ),
+              ]),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
